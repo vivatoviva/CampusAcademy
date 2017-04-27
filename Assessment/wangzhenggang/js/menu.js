76be6017efb46fd7efcacdd785d1a47029b84302
ParaSLee <610033941@qq.com>
2017-04-27 23:11:08 +0800

    王正刚
    王正刚

---
+++ Assessment/wangzhenggang/js/menu.js
@@ -0,0 +1,24 @@
+function list()
+		{
+			var a=document.getElementById("hide");
+			if (a.style.display=="none") 
+				{
+					a.style.display="block";
+				}
+			else{
+					a.style.display="none";
+				}
+
+		}
+		function list2()
+		{
+			var b=document.getElementById("document2");
+			if (b.style.display=="none") 
+				{
+					b.style.display="block";
+				}
+			else{
+					b.style.display="none";
+				}
+
+		}
